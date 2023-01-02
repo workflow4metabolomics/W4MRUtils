7b9cd7c23b4b96b2083f59eda7ed53c6f4b2238e
Lain Pavot <lain.pavot@inrae.fr>
2023-01-02 16:11:53 +0100

    test(unit:stock/reproduce id): add tests for stock and reproduce ID functions

--- tests/testthat/test-miniTools.R
+++ tests/testthat/test-miniTools.R
@@ -36,34 +36,36 @@ test_that("Testing parse_args", {
   })
 })
 
-test_that("Testing stock_id", {
-  data_matrix <- data.frame(id = 1:9, a = 2:10, b = 3:11)
-  var_meta <- data.frame(x = 1:9, y = 2:10, z = 3:11)
+test_that("Testing stock_id - variable", {
+  data_matrix <- data.frame(id = c("a", "b"), a = 2:3, b = 3:4)
+  var_meta <- data.frame(a = 1:2, b = 2:3, c = 3:4)
   testthat::expect_identical(
     stock_id(data_matrix = data_matrix, var_meta, "variable"),
     list(
       id.match = data.frame(
-        order.ori = 1:9,
-        x = 1:9,
-        id.new = make.names(1:9),
-        id = 1:9,
-        id.new.DM = make.names(1:9)
+        order.ori = 1:2,
+        a = 1:2,
+        id.new = make.names(c("X1", "X2")),
+        id = c("a", "b"),
+        id.new.DM = make.names(c("a", "b"))
       ),
       dataMatrix = data.frame(
-        id = make.names(1:9),
-        a = 2:10,
-        b = 3:11
+        id = make.names(c("a", "b")),
+        a = 2:3,
+        b = 3:4
       ),
       Metadata = data.frame(
-        x = make.names(1:9),
-        y = 2:10,
-        z = 3:11
+        a = make.names(c("X1", "X2")),
+        b = 2:3,
+        c = 3:4
       )
     )
   )
 })
 
-test_that("Testing reproduce_id", {
+test_that("Testing reproduce_id - variable", {
+  data_matrix <- data.frame(id = c("a", "b"), a = 2:3, b = 3:4)
+  var_meta <- data.frame(a = 1:2, b = 2:3, c = 3:4)
 
   datamatrix_a <- data.frame(
     data = 1:6, a = 2:7, b = 3:8, c = 2:7, d = 3:8, e = 2:7
@@ -80,4 +82,47 @@ test_that("Testing reproduce_id", {
   reproduced <- reproduce_id(datamatrix, variable_metadata, "variable", stored)
   testthat::expect_identical(datamatrix_a, reproduced$dataMatrix)
   testthat::expect_identical(variablemeta_a, reproduced$Metadata)
+})
+
+test_that("Testing stock_id - sample", {
+  data_matrix <- data.frame(id = 1:3, a = 2:4, b = 3:5, c = 4:6)
+  sample_meta <- data.frame(x = 1:3, y = 2:4, z = 3:5)
+  testthat::expect_identical(
+    stock_id(data_matrix = data_matrix, sample_meta, "sample"),
+    list(
+      id.match = data.frame(
+        order.ori = 1:3,
+        x = 1:3,
+        id.new = make.names(1:3),
+        id = c("a", "b", "c"),
+        id.new.DM = c("a", "b", "c")
+      ),
+      dataMatrix = data.frame(
+        id = 1:3,
+        a = 2:4,
+        b = 3:5,
+        c = 4:6
+      ),
+      Metadata = data.frame(
+        x = make.names(1:3),
+        y = 2:4,
+        z = 3:5
+      )
+    )
+  )
+})
+
+test_that("Testing reproduce_id - sample", {
+
+  datamatrix_a <- data.frame(id = c("a", "b", "c"), a = 2:4, b = 3:5, c = 4:6)
+  sample_meta_a <- data.frame(a = 1:3, b = 2:4, c = 3:5)
+  stored <- stock_id(datamatrix_a, sample_meta_a, "sample")
+
+  datamatrix <- as.data.frame(stored$dataMatrix)
+  sample_metadata <- as.data.frame(stored$Metadata)
+  stored <- stored$id.match
+
+  reproduced <- reproduce_id(datamatrix, sample_metadata, "sample", stored)
+  testthat::expect_identical(reproduced$dataMatrix, datamatrix_a)
+  testthat::expect_identical(reproduced$Metadata, sample_meta_a)
 })
