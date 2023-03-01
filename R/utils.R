1523a14bed5d4a1b139f841f24fef42c909ec820
Lain Pavot <lain.pavot@inrae.fr>
2023-03-01 20:15:55 +0100

    feat(utils:creation): + a utility set to help develop more stable tools

---
+++ R/utils.R
@@ -0,0 +1,106 @@
+
+stopf <- function(...) {
+  stop(sprintf(...))
+}
+
+stopaste <- function(...) {
+  stop(paste(...))
+}
+
+stopaste0 <- function(...) {
+  stop(paste0(...))
+}
+
+printf <- function(...) {
+  print(sprintf(...))
+}
+
+printp <- function(...) {
+  print(paste(...))
+}
+
+check_param_type_n_length <- function(
+  value,
+  expected_type,
+  expected_size = 1,
+  nth = NULL,
+  func_name = NULL,
+  param_name = NULL
+) {
+  if (is.null(param_name)) {
+    param_name <- deparse(substitute(value))
+  }
+  if (is.null(func_name)) {
+    func_name <- deparse(sys.calls()[[sys.nframe() - 1]][[1]])
+  }
+  check_parameter_type(
+    value,
+    expected_type,
+    nth = nth,
+    func_name = func_name,
+    param_name = param_name
+  )
+  check_parameter_length(
+    value,
+    expected_size,
+    nth = nth,
+    func_name = func_name,
+    param_name = param_name
+  )
+}
+
+check_parameter_type <- function(
+  value,
+  expected_type,
+  nth = NULL,
+  func_name = NULL,
+  param_name = NULL
+) {
+  if (is.null(param_name)) {
+    param_name <- deparse(substitute(value))
+  }
+  if (is.null(func_name)) {
+    func_name <- deparse(sys.calls()[[sys.nframe() - 1]][[1]])
+  }
+  if (! is.null(nth) && is.character(nth) && length(nth) == 1) {
+    nth <- sprintf(" (%s)", nth)
+  } else {
+    nth <- ""
+  }
+  if (!is(value, expected_type)) {
+    stopf(
+      "The '%s'%s parameter for %s must be a %s, not a %s.",
+      param_name,
+      nth,
+      func_name,
+      expected_type,
+      paste(class(value), collapse = "/")
+    )
+  }
+}
+
+check_parameter_length <- function(
+  value,
+  expected_size,
+  nth = NULL,
+  func_name = NULL,
+  param_name = NULL
+) {
+  if (is.null(param_name)) {
+    param_name <- deparse(substitute(value))
+  }
+  if (is.null(func_name)) {
+    func_name <- deparse(sys.calls()[[sys.nframe() - 1]][[1]])
+  }
+  if (! is.null(nth) && is.character(nth) && length(nth) == 1) {
+    nth <- sprintf(" (%s)", nth)
+  } else {
+    nth <- ""
+  }
+  if (length(value) != expected_size) {
+    stopf(
+      "The '%s'%s parameter for %s must be %s element long, not %s.",
+      param_name, nth, func_name, expected_size, length(value)
+    )
+  }
+}
