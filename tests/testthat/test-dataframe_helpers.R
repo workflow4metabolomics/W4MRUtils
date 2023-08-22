505a691d31f2b82c26b73c117db9dc090481a23d
Lain Pavot <lain.pavot@inrae.fr>
2023-08-22 15:07:12 +0200

    test(refacto:dataframe): df helpers tests refactored

--- tests/testthat/test-dataframe_helpers.R
+++ tests/testthat/test-dataframe_helpers.R
@@ -1,13 +1,29 @@
 
+
+write_read_df <- function(
+  ...,
+  write_args = list(),
+  read_args = list()
+) {
+  df <- do.call(data.frame, list(...))
+  path <- tempfile()
+  file.create(path)
+  write_args$x <- df
+  write_args$file <- path
+  do.call(write.table, write_args)
+  read_args$file <- path
+  return(suppressWarnings(
+    list(
+      original = df,
+      actual = do.call(df_read_table, read_args)
+    )
+  ))
+}
+
+
 test_that("Testing df_is", {
-  testthat::expect_true(df_is(
-    data.frame(a = c(1)),
-    "numeric"
-  ))
-  testthat::expect_false(df_is(
-    data.frame(a = c(1)),
-    "character"
-  ))
+  testthat::expect_true(df_is(data.frame(a = c(1)), "numeric"))
+  testthat::expect_false(df_is(data.frame(a = c(1)), "character"))
   testthat::expect_false(df_is(
     data.frame(a = c(1), b = c("test")),
     "numeric"
@@ -64,59 +80,49 @@ test_that("Testing df_force_numeric", {
 })
 
 test_that("Testing df_read_table", {
-  original_df <- data.frame(a = c(6, 7, 8), b = c(4, 5, 6))
-  path <- tempfile()
-  file.create(path)
-  write.table(original_df, file = path)
-  suppressWarnings({
-    loaded_df <- df_read_table(path, force_numeric = TRUE)
-  })
-  testthat::expect_identical(loaded_df, original_df)
-
-  original_df <- data.frame(a = c(6, 7, 8), b = c(4, 5, 6))
-  path <- tempfile()
-  file.create(path)
-  write.table(original_df, file = path)
-  suppressWarnings({
-    loaded_df <- df_read_table(path, force_numeric = FALSE)
-  })
-  testthat::expect_false(identical(loaded_df, original_df))
-
-  ## FIXME: This tests should work
-  # original_df <- data.frame(a = c(6, 7, 8), b = c("4", "5", "6"))            ##nolint
-  # path <- tempfile()                                                         ##nolint
-  # file.create(path)                                                          ##nolint
-  # write.table(original_df, file = path, row.names = FALSE)                   ##nolint
-  # loaded_df <- df_read_table(path, force_numeric = c("a"), header = TRUE)    ##nolint
-  # testthat::expect_identical(loaded_df, original_df)                         ##nolint
-
-  original_df <- data.frame(a = c("7", "8", "9"), b = c(4, 5, 6))
-  path <- tempfile()
-  file.create(path)
-  write.table(
-    original_df,
-    file = path,
-    row.names = FALSE
+  result <- write_read_df(
+    a = c(6, 7, 8), b = c(4, 5, 6),
+    read_args = list(force_numeric = TRUE)
   )
-  suppressWarnings({
-    loaded_df <- df_read_table(
-      path,
+  testthat::expect_identical(result$original, result$actual)
+
+  result <- write_read_df(
+    a = c(6, 7, 8), b = c(4, 5, 6),
+    read_args = list(force_numeric = FALSE)
+  )
+  testthat::expect_false(identical(result$original, result$actual))
+
+  result <- write_read_df(
+    a = c("7", "8", "9"), b = c(4, 5, 6),
+    read_args = list(
       colClasses = c(a = "character", b = "numeric"),
       header = TRUE
-    )
-  })
-  testthat::expect_identical(loaded_df, original_df)
+    ),
+    write_args = list(row.names = FALSE)
+  )
+  testthat::expect_identical(result$original, result$actual)
 
-  original_df <- data.frame(a = c("7", "8", "9"), b = c("4", "5", "6"))
-  path <- tempfile()
-  file.create(path)
-  write.table(original_df, file = path, row.names = FALSE)
-  suppressWarnings({
-    loaded_df <- df_read_table(
-      path,
+  result <- write_read_df(
+    a = c("7", "8", "9"), b = c(4, 5, 6),
+    write_args = list(row.names = FALSE),
+    read_args = list(
+      colClasses = c(a = "character", b = "numeric"),
+      header = TRUE,
+      force_numeric = "a"
+    )
+  )
+  testthat::expect_identical(
+    data.frame(a = c(7, 8, 9), b = c(4, 5, 6)),
+    result$actual
+  )
+
+  result <- write_read_df(
+    a = c("7", "8", "9"), b = c("4", "5", "6"),
+    write_args = list(row.names = FALSE),
+    read_args = list(
       colClasses = c(a = "character", b = "character"),
       header = TRUE
     )
-  })
-  testthat::expect_identical(loaded_df, original_df)
+  )
+  testthat::expect_identical(result$original, result$actual)
 })
