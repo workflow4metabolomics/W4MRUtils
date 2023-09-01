aa035484c7de998669077a1a0bc347f0871c7994
Lain Pavot <lain.pavot@inrae.fr>
2023-09-01 14:52:42 +0200

    test(utils:*): creation of tests for utilities functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -56,6 +56,66 @@ testthat::test_that("Parameter checkers works fine", {
   }
   testthat::expect_null(uwu(""))
   testthat::expect_null(òwó(""))
-  testthat::expect_error(uwu(42))
-  testthat::expect_error(òwó(c("", "")))
+  testthat::expect_error(
+    uwu(42),
+    regexp = "The 'x' parameter for uwu must be a character, not a numeric.",
+    fixed = TRUE
+  )
+  testthat::expect_error(
+    òwó(c("", "")),
+    regexp = "The 'x' parameter for òwó must be 1 element long, not 2.",
+    fixed = TRUE
+  )
+
+  x <- function(text, logical, number) {
+    check_one_character(text)
+    check_one_logical(logical)
+    check_one_numeric(number)
+  }
+  testthat::expect_error(
+    x(1, TRUE, 12),
+    regexp = "The 'text' parameter for x must be a character, not a numeric.",
+    fixed = TRUE
+  )
+  testthat::expect_error(
+    x("1", "TRUE", 12L),
+    regexp = "The 'logical' parameter for x must be a logical, not a character",
+    fixed = TRUE
+  )
+  testthat::expect_error(
+    x("1", TRUE, "12L"),
+    regexp = "The 'number' parameter for x must be a numeric, not a character.",
+    fixed = TRUE
+  )
+
+  ## checking each function generated programmatically that does
+  ## parameters checking
+  check_one_list_of_type <- list(
+    complex = complex(imaginary = 42),
+    character = "uwu",
+    numeric = 1.5,
+    integer = as.integer(1.5),
+    logical = TRUE
+  )
+  for (type_name in names(check_one_list_of_type)) {
+    checker <- get(sprintf("check_one_%s", type_name))
+    value <- check_one_list_of_type[[type_name]]
+    testthat::expect_no_error(
+      (function(x) checker(x, or_null = TRUE))(NULL)
+    )
+    testthat::expect_no_error(
+      (function(x) checker(x, or_null = TRUE))(value)
+    )
+    testthat::expect_no_error(
+      (function(x) checker(x, or_more = TRUE))(c(value, value))
+    )
+    testthat::expect_error(
+      (function(x) checker(x))(c(value, value)),
+      regexp = paste0(
+        "The 'x' parameter for (function(x) checker(x)) ",
+        "must be 1 element long, not 2."
+      ),
+      fixed = TRUE
+    )
+  }
 })
